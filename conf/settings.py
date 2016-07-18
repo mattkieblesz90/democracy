48d11c933cc3db2a8e1ae17f1ec4cccc434e32f7
Matt K <mattkieblesz@hotmail.com>
2016-07-18 22:31:57 +0100

    Fix remote collecting static files

--- conf/settings.py
+++ conf/settings.py
@@ -128,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.environ['STATIC_ROOT']
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'src/django/static'),
 ]
