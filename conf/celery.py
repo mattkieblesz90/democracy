36196f45b2077fbcf5677e8765b34032a35f474a
Matt K <mattkieblesz@hotmail.com>
2016-07-19 01:49:03 +0100

    Fix celery app

--- conf/celery.py
+++ conf/celery.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import, unicode_literals
 
 import os
+import sys
 
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
 
 from django.conf import settings  # noqa
 
@@ -13,11 +14,11 @@ app = Celery('democracy')
 
 # Using a string here means the worker does not have to serialize
 # the configuration object.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object('django.conf:settings')
 
+sys.path.append(os.path.join(settings.BASE_DIR, 'src/django/apps'))
 # load task modules from all registered Django app configs.
-app.autodiscover_tasks()
-
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 # @app.task(bind=True)
 # def debug_task(self):
