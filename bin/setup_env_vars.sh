560410985a564ea12e0edaedd6c3308aecc6ba34
Matt K <mattkieblesz@hotmail.com>
2016-07-20 17:49:00 +0100

    Provision succesfully with ssl cert and key

--- bin/setup_env_vars.sh
+++ bin/setup_env_vars.sh
@@ -9,3 +9,4 @@ export STATIC_ROOT=/home/mat/Projects/democracy/collected_static
 # for ansible file encryption
 export ANSIBLE_VAULT_PASSWORD_FILE=~/.vault_pass.txt
 export ssh_pass="yvlsQ>Zb>D+2I&1\k#Z*XEo/5vu|H1asKypd#Zlxd4;PW+).R-Z?^q'9)X/}_c#"
+export ssl_pass='e$R56h?K^r+3jD+RzkJbRTCZZ4n3%QnwD59TKFYUyd!6pDp=uCTaeeVBMa5#WD$b6Y5gTy-LD&7_Fy@8r5LV!3+AcBaxkyy8yqek*^RWrjjD@!=bJ7G@Y^Dv$V#x_pYRKH-6Cg3?3q^ap742JvRRfXQLuSQ5r=M?^Gd&JY3Sh7EC^P&AvC_jQRt=W?qPs#L9+ukJ6e@SGB^djyAVXxU#fLFu2wP%sWm695T-4JKYD*cQ_6bntAze7KK8Uu2=u-uy'
